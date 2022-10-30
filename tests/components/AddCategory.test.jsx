ed744af9ddee1e18539abbf5211ce4702651cb00
Eduardo <eduu.ugalde@gmail.com>
2022-10-29 23:01:36 -0500

    AddCategory test agregado

--- tests/components/AddCategory.test.jsx
+++ tests/components/AddCategory.test.jsx
@@ -9,7 +9,44 @@ describe('Pruebas en AddCategory', () => {
         fireEvent.input(input,{target: {value: 'Valorant'}});
         
         expect( input.value ).toBe( 'Valorant' )
-        screen.debug();
-     });
+        //screen.debug();
+    });
 
+
+     test('Debe llamar a onNewCategory si el input tiene un valor', () => { 
+        const inputValue    = 'Valorant';
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input,{target: {value: 'Valorant'}});
+        fireEvent.submit(form);
+        //screen.debug();
+
+        expect(input.value).toBe('');
+        expect( onNewCategory ).toHaveBeenCalled();
+        expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        expect( onNewCategory ).toHaveBeenCalledWith(inputValue);
+
+        
+    });
+
+    test('No debe de llamar a on new category si el input está vacío', () => {
+            
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+        
+        const form = screen.getByRole('form');
+        fireEvent.submit(form);
+
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
+
+        
+
+    });
  });
